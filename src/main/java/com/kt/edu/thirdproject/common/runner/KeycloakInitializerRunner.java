193e1f9d161b89d897db9f368afd3bb8713d54e9
kt <ujeongeom@ktui-MacBookPro.local>
2024-02-21 13:50:32 +0900

    local->vm

--- src/main/java/com/kt/edu/thirdproject/common/runner/KeycloakInitializerRunner.java
+++ src/main/java/com/kt/edu/thirdproject/common/runner/KeycloakInitializerRunner.java
@@ -24,7 +24,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Component
 // KeycloakInitializerRunner 는 realm 생성하고 삭제 반복. 주석 처리하고 수정 생성
-public class KeycloakInitializerRunner {//implements CommandLineRunner {
+public class KeycloakInitializerRunner implements CommandLineRunner {
 
     private final Keycloak keycloakAdmin;
 
